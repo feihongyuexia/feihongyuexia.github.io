b0f5e59bff43b5ec21d6ff0b4148f9cfd8f6b025
feihongyuexia <95088881+feihongyuexia@users.noreply.github.com>
2024-12-11 17:19:13 +0800

    Add files via upload

--- Reload_bate2/js/load_video.js
+++ Reload_bate2/js/load_video.js
@@ -1,9 +1,11 @@
-// 接收video元素
-const video = document.getElementById('video-background');
-// 接收loading元素
-const loading = document.getElementById('loading'); 
-// 接收video的父元素
-video.addEventListener('load', function() { 
-    loading.style.display = 'none'; // 隐藏loading元素
-    video.style.display = 'block';  // 显示video元素
-});
+window.onload = function() {
+    const video = document.getElementById('video-background'); // 获取视频元素
+    const content = document.getElementById('content'); // 获取页面内容
+    const loading = document.getElementById('loading'); // 获取加载提示
+
+    // 监听视频加载事件
+    video.onloadeddata = function() {
+        content.style.display = 'block'; // 显示页面内容
+        loading.style.display = 'none';  // 隐藏加载提示
+    };
+};
